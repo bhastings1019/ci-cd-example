0b23b190d3d2bc009cff991f360302d2657e132f
Ben Hastings <ben@hastings.cc>
2023-11-29 17:37:27 -0800

    feat: call add function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import packageJson from '../package.json';
 
 function main() {
   console.log(`${packageJson.description} version: ${packageJson.version}`);
+  
+  add(1, 2);
 }
 
 function add(a, b) {
